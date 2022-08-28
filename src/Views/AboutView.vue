a81bedf37ab12407537513b76ca8b1118f53663d
ThomasPeterVB <thomaspeterbusiness@gmail.com>
2022-08-28 21:02:08 +0200

    some design changes

--- src/Views/AboutView.vue
+++ src/Views/AboutView.vue
@@ -2,11 +2,11 @@
   <div class="aboutcontainer">
     <img @mouseover="colorMe" src="@/assets/images/thomaspetergreyscale.png" alt="">
     <div class="topright">
-      <h1><VueWriter class="inline" :typeSpeed="100" :eraseSpeed="50"  :array="['JavaScript', 'TypeScript', 'C#', 'Vue', 'React', 'Node', 'Python', 'Java', 'C++']">Hi! I'm <ClickableText text="Thomas" :startSelection="5" />,<br>and I'm a </VueWriter>developer</h1>
+      <h1><VueWriter class="inline" :typeSpeed="100" :eraseSpeed="50"  :array="[ '.NET', 'JavaScript', 'Unity', 'Vue', 'React', 'Node', 'MonoGame', 'Python', 'TypeScript']">Hi! I'm <ClickableText text="Thomas" :startSelection="5" />,<br>and I'm a </VueWriter>developer</h1>
       <br><br>
-      <p>I'm a Freelance full stack web and tools developer from northern Sweden, currently building customer experience tools for a leading global music platform. I gained my first experience in the industry developing an MMORPG, and ran it from my bedroom for 4 years while attending highschool.</p>
+      <p>I'm a Freelance full stack web and software developer from northern Sweden, currently building customer experience tools for a leading global music platform. I gained my first experience in the industry developing an MMORPG, and ran it from my bedroom for 4 years while attending highschool.</p>
       <br><br>
-      <p>My passion for programming, or rather development as a whole, derives from a desire to create and perfect, therefore my products are UX-centered, intuitive, and highly-optimised.</p>
+      <p>My passion for programming, or rather development as a whole, derives from a desire to create and improve, therefore my products are UX-centered, intuitive, and highly-optimised.</p>
     </div>
     <div class="bottomrow">
       <a target="_blank" href="#"><svg xmlns="http://www.w3.org/2000/svg" viewBox="-2 -2 24 24" width="24" fill="currentColor"><path d="M19.959 11.719v7.379h-4.278v-6.885c0-1.73-.619-2.91-2.167-2.91-1.182 0-1.886.796-2.195 1.565-.113.275-.142.658-.142 1.043v7.187h-4.28s.058-11.66 0-12.869h4.28v1.824l-.028.042h.028v-.042c.568-.875 1.583-2.126 3.856-2.126 2.815 0 4.926 1.84 4.926 5.792zM2.421.026C.958.026 0 .986 0 2.249c0 1.235.93 2.224 2.365 2.224h.028c1.493 0 2.42-.989 2.42-2.224C4.787.986 3.887.026 2.422.026zM.254 19.098h4.278V6.229H.254v12.869z"></path></svg></a>
